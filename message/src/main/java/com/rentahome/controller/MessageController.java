db6c72654431be08635f42cdd9d36ed8a4aaef34
laureneddd <102869786+laureneddd@users.noreply.github.com>
2024-08-08 21:11:55 -0700

    Add files via upload

--- message/src/main/java/com/rentahome/controller/MessageController.java
+++ message/src/main/java/com/rentahome/controller/MessageController.java
@@ -1,8 +1,47 @@
 package com.rentahome.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import com.rentahome.entity.Message;
+import com.rentahome.service.MessageService;
+import com.rentahome.service.ReplyService;
+
+
+
+@Controller
+@RequestMapping("/message/")
 public class MessageController {
+    
+    @Autowired
+    MessageService messageService;
+
+    @Autowired 
+    ReplyService replyService;
+
+    @GetMapping("/send")
+    public ModelAndView showSendMessageForm() {
+        return new ModelAndView("add_message");
+    }
+
+    @PostMapping("/addmessage")
+    public ModelAndView addMessage(@ModelAttribute Message message) {
+        messageService.addMessage(message);
+        ModelAndView modelAndView = new ModelAndView("add_message");
+        modelAndView.addObject("messageSuccess", "Message added successfully!");
+        return modelAndView;
+    }
+
+    // @GetMapping("/user_messages")
+    // public ModelAndView getUserMessages(@RequestParam String userId) {
+    //     ModelAndView modelAndView = new ModelAndView("message_page");
+    //     // modelAndView.addObject("messages", messageService.getMessagesByUserId(userId));
+    //     return modelAndView;
+    // }
     
 }
---
+++ message/src/main/java/com/rentahome/controller/ReplyController.java
@@ -0,0 +1,5 @@
+package com.rentahome.controller;
+
+public class ReplyController {
+    
+}
